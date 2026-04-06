7769603095bcb44166e5ccde5b4bfb9ee1f9a0f2
Murilo Dionizio - Luis Eduardo <murilomelodionizio@gmail.com>
2026-04-06 15:45:18 -0300

    Estrutura do projeto

---
+++ main.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+Base = declarative_base()
+
+
